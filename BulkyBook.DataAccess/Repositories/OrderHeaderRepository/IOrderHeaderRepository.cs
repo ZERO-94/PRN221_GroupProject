5b8227f00efafd111b8d9ffde146c2a3fd744d4a
AnhTVK <70008423+ZERO-94@users.noreply.github.com>
2023-02-07 18:16:51 -0800

    init

---
+++ BulkyBook.DataAccess/Repositories/OrderHeaderRepository/IOrderHeaderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BulkyBook.DataAccess.Repositories.BaseRepository;
+using BulkyBook.BusinessObject.Models;
+
+namespace BulkyBook.DataAccess.Repositories.OrderHeaderRepository
+{
+	public interface IOrderHeaderRepository: IBaseRepository<OrderHeader>	{
+		public void UpdateShippingStatus(string trackingNumber, string carrier);
+		public void UpdateStatus(string orderStatus);
+	}
+}
+
---
+++ BulkyBook.DataAccess/Repositories/OrderHeaderRepository/OrderHeaderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using BulkyBook.BusinessObject.Models;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repositories.BaseRepository;
+using BulkyBook.DataAccess.Repositories.OrderHeaderRepository;
+
+namespace BulkyBook.DataAccess.Repositories.OrderRepository
+{
+	public class OrderHeaderRepository: BaseRepository<ApplicationDbContext, OrderHeader>, IOrderHeaderRepository
+	{
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<OrderHeader> _dbSet;
+        public CoverTypeRepository(ApplicationDbContext context) : base(context)
+        {
+            _dbContext = context;
+            _dbSet = _dbContext.Set<OrderHeader>();
+        }
+
+        public void UpdateShippingStatus(string trackingNumber, string carrier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStatus(string orderStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
